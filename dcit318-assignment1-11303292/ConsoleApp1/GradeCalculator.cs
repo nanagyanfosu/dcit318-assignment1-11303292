59944c326eca3f9ad29022bc9eb9a9406bb189f5
nanagyanfosu <nanagyanfosu@gmail.com>
2025-07-31 00:19:33 +0000

    Grade Calculator task done

---
+++ dcit318-assignment1-11303292/ConsoleApp1/GradeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class GradeCalculator
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the Grade Calculator!");
+        Console.Write("Enter your score (0-100):");
+        string? initialScore = Console.ReadLine();
+
+        if (int.TryParse(initialScore, out int score) && score >= 0 && score <= 100)
+        {
+            string grade = score switch
+            {
+                >= 90 => "A",
+                >= 80 => "B",
+                >= 70 => "C",
+                >= 60 => "D",
+                _ => "F"
+            };
+            Console.WriteLine($"Your grade is " + grade);
+        }
+        else
+        {
+            Console.WriteLine("Invalid score. Enter a score between 0 and 100.");
+        }
+
+    }
+}
+
