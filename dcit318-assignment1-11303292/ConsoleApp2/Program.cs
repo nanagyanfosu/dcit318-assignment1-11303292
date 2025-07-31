e807fe1bc69b4261185197e5038bdf4175169c55
nanagyanfosu <nanagyanfosu@gmail.com>
2025-07-31 23:00:59 +0000

    Movie Ticket Price Calculator Done

---
+++ dcit318-assignment1-11303292/ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+     class TicketCalculator
+    {
+        static void Main()
+        {
+
+        double ticket = 10.0;
+        Console.WriteLine("***********Ticket Price Calculator*******");
+
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        if (age >= 65 || age <= 12)
+        {
+            ticket = ticket - (ticket * 30 / 100);  //GHs 7 is a 30% discount
+
+        }
+        else
+        {
+          
+        }
+        Console.WriteLine("You will pay GHS " + ticket + "for your ticket.");
+    }
+    }
